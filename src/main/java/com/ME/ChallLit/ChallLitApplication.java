c59895f682dee5e9432e2d4ce4eb62befff73b70
nilson <nilsonjmn21@gmail.com>
2024-07-04 21:34:26 -0500

    comm

---
+++ src/main/java/com/ME/ChallLit/ChallLitApplication.java
@@ -0,0 +1,13 @@
+package com.ME.ChallLit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallLitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallLitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ME/ChallLit/ChallLitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ME.ChallLit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallLitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
